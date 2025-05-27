5eb9edc3b196b2b395ea4126a25206e571fecab9
William Blake Galbreath <blake.galbreath@gmail.com>
2025-05-27 12:51:04 -0500

    make site title fade in on load this is so we don't see it blink when the font loads

--- docs/_includes/js/custom.js
+++ docs/_includes/js/custom.js
@@ -17,3 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+window.addEventListener('load', function() {
+  document.getElementsByClassName("site-title")[0].style.opacity = 1;
+});
